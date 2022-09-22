63b704fb866408a741d581f4a2d413d1f48f0e35
Maksim Kushner <113466296+MaxZzzIt@users.noreply.github.com>
2022-09-22 22:39:02 +0300

    Конвертация в UTF-8

--- src/by/home/maxzzzit/VivodTekusheiDateTime.java
+++ src/by/home/maxzzzit/VivodTekusheiDateTime.java
@@ -3,11 +3,11 @@ package by.home.maxzzzit;
 import java.util.Date;
 import java.util.Locale;
 
-// Вывод текущей даты и времени по шаблону
+// Р’С‹РІРѕРґ С‚РµРєСѓС‰РµР№ РґР°С‚С‹ Рё РІСЂРµРјРµРЅРё РїРѕ С€Р°Р±Р»РѕРЅСѓ
 public class VivodTekusheiDateTime {
 	public static void main(String[]args) {
 			Locale locale_ru_RU = new Locale("ru", "RU");
-			System.out.printf(locale_ru_RU,	"Сейчас время в нашей стране:\n"
+			System.out.printf(locale_ru_RU,	"РЎРµР№С‡Р°СЃ РІСЂРµРјСЏ РІ РЅР°С€РµР№ СЃС‚СЂР°РЅРµ:\n"
 			+ "%1$td %1$tB %1$tY %1$tH:%1$tM:%1$tS\n"
 			+ "%1$td.%1$tm.%1$tY", new Date());
 	}
